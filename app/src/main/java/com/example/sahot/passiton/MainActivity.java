43766fc31ef98005aa70429f6ae8ac70e180b81c
PS1718 <sahotap1@hotmail.com>
2018-03-25 16:56:15 -0600

    logorefresh
    i changed a couple things

--- app/src/main/java/com/example/sahot/passiton/MainActivity.java
+++ app/src/main/java/com/example/sahot/passiton/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sahot.passiton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        ImageButton btn = (ImageButton)findViewById(R.id.preetsbutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+
+
+
+
     }
 }
