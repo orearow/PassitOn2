02b0dc5445a1d31604042b4f0b30c04366c53cac
PS1718 <sahotap1@hotmail.com>
2018-04-07 13:30:07 -0600

    Preets most recent changes

--- app/src/main/java/com/example/sahot/passiton/MainActivity.java
+++ app/src/main/java/com/example/sahot/passiton/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                startActivity(new Intent(MainActivity.this, RecentUploads.class));
             }
         });
 
