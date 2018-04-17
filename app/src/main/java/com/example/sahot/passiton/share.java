6beba518d6275f6bf86e2189a2dac40860667d1e
Arti-Garchar <36177053+Arti-Garchar@users.noreply.github.com>
2018-04-17 15:56:16 -0600

    arti's changes

--- app/src/main/java/com/example/sahot/passiton/share.java
+++ app/src/main/java/com/example/sahot/passiton/share.java
@@ -1,7 +1,11 @@
 package com.example.sahot.passiton;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class share extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class share extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+
+
+        ImageButton recent_upload = (ImageButton) findViewById(R.id.imageButton12);
+
+        recent_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(share.this, camera.class));
+                //startActivity(new Intent(share.this, camera.class));
+            }
+        });
+
+
     }
 }
